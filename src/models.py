555b10d5e9531ddca1ea77cba71dc0d752710ea0
brcre001 <brandon.f.creed@gmail.com>
2021-08-09 18:22:38 +0000

    added appropriate tables with keys

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
-import os
+import os, datetime
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,146 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(80), nullable=False)
+    join_date = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+            "join_date": self.join_date
+        }
+
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    height = Column(String(80), nullable=False)
+    mass = Column(String(80), nullable=False)
+    hair_color = Column(String(80), nullable=True)
+    skin_color = Column(String(80), nullable=True)
+    eye_color = Column(String(80), nullable=True)
+    birth_year = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=True)
+    created = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    edited = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    homeworld = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "created": self.created,
+            "edited": self.edited,
+            "homeworld": self.homeworld,
+            "url": self.url
+        }
+
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer, nullable=False)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    gravity = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)
+    created = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    edited = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    url = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "created": self.created,
+            "edited": self.edited,
+            "url": self.url
+        }
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cost_in_credits = Column(Integer, nullable=False)
+    length = Column(Integer, nullable=False)
+    crew = Column(Integer, nullable=False)
+    passengers = Column(Integer, nullable=False)
+    max_atmosphering_speed = Column(Integer, nullable=False)
+    hyperdrive_rating = Column(String(250), nullable=False)
+    MGLT = Column(Integer, nullable=False)
+    cargo_capacity = Column(Integer, nullable=False)
+    consumables = Column(String(250), nullable=False)
+    pilots = Column(String(250), nullable=False)
+    created = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    edited = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    url = Column(String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "startship_class": self.starship_class,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "hyperdrive_rating": self.hyperdrive_rating,
+            "MGLT": self.MGLT,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "pilots": self.pilots,
+            "created": self.created,
+            "edited": self.edited,
+            "url": self.url
+        }
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey('users.id'))
+    favorite_name = Column(String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "favorite_name": self.favorite_name
+        }
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
